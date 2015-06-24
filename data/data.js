c5779f26c474e081b588ab8c1cf1b3490e195f60
Ben Sainsbury <redtrails@gmail.com>
2015-06-24 14:39:12 -0700

    Initial commit

---
+++ data/data.js
@@ -0,0 +1,169 @@
+{
+	"amenities": {
+		"private": ["Bakery",
+			"Bar",
+			"Bike Shop",
+			"Bookstore",
+			"Brewpub",
+			"Child Care",
+			"Cinema",
+			"Clothing Store",
+			"Coffee Shop",
+			"Department Store",
+			"Dry Cleaners",
+			"Fitness Gym",
+			"Grocery Store",
+			"Music Store",
+			"Restaurant",
+			"Specialty Snacks & Beverages"
+		],
+		"public": [
+			"Community Center",
+			"Fire Station",
+			"Govt. Building",
+			"Library",
+			"School"
+		]
+		
+	},
+	"income_breaks": [
+			"<$15,000",
+			"$15,000-$34,999",
+			"$35,000-$49,999",
+			"$50,000-$74,999",
+			"$75,000-$99,999",
+			"$100,000 +"
+		],
+		"town_center_averages":{
+			"total_pop": 2326,
+			"avg_HH_size": 2.42,
+			"med_HH_income": 60133,
+			"med_age": 36.0,
+			"pct_home_ownership": 47.4,
+			"people_per_acre": 20.1,
+			"du_per_acre": 5.0,
+			"total_biz_acre": 0.72,
+			"total_num_employees": 1745,
+			"market_value": 39,
+			"net_acreage": 222,
+			"total_du": 1073,
+			"total_biz": 120
+		},
+		"regional_center_averages":{
+			"total_pop": 2326,
+			"avg_HH_size": 2.42,
+			"med_HH_income": 60133,
+			"med_age": 36.0,
+			"pct_home_ownership": 47.4,
+			"people_per_acre": 20.1,
+			"du_per_acre": 5.0,
+			"total_biz_acre": 0.72,
+			"total_num_employees": 1745,
+			"market_value": 39,
+			"net_acreage": 222,
+			"total_du": 1073,
+			"total_biz": 120
+		},
+		"data_years":[2011, 2013, 2015],
+		"context_tool_labels" : ["Parks Access", "Transit Access", "Bike route density", "People per acre", "Sidewalk density", "Private amenities", "Block size"],
+		"Aloha": {
+			"title":"Aloha",
+			"type": "Town Center",
+			"narrative": "The Aloha Town Center is located along Tualatin Valley Highway, running north and south along 185th Avenue, within Washington County. The center has one ODOT facility within its boundary: Southwest Tualatin Valley Highway (State Highway 8), which connects Beaverton to Hillsboro. Auto-oriented retail locations serve the surrounding community along Southwest Tualatin Valley Highway. The center has 6,610 residents, 1,000 employees and 2,520 dwelling units. Aloha Town Center contains 511 gross acres.",
+			"private_amenities": [3, 2, 0, 0, 0, 2, 0, 0, 0, 0, 1, 0, 6, 0, 17, 1],
+			"public_amenities": [0,0,0,0,1],
+			"incomes":[
+						[13.0,20.9,18.5,20.1,13.3,14.2],
+						[14.0,13.9,17.5,15.1,3.3,20.2],
+						[12.0,13.9,15.5,17.1,20.3,17.2]
+					],
+			"incomes_1mi_buff": [
+						[6.9,13.8,16.2,26.8,16.1,19.3],
+						[7.9,12.8,17.2,20.8,14.1,20.3],
+						[8.9,14.8,14.2,13.8,18.1,16.3]
+					],
+			"emp_breakdown":[
+						[22,39,39],
+						[18,32,30],
+						[24,43,35]
+						],
+			"context_tool_scores":[
+				[1,3,5,1,5,2,3],
+			   	[4,5,3,1,4,1,4],
+			   	[3,5,3,4,3,1,1]
+			],
+			"stats":{
+				"total_pop": 6611,
+				"avg_HH_size": 2.91,
+				"med_HH_income": 48053,
+				"med_age": 29.3,
+				"pct_home_ownership": 38,
+				"people_per_acre": 18.2,
+				"du_per_acre": 6,
+				"total_biz_acre": 0.28,
+				"total_num_employees": 1003,
+				"market_value": 26,
+				"net_acreage": 419,
+				"total_du": 2520,
+				"total_biz": 118,
+				"gross_acres": 511
+			},
+			"stats_1mi_buff":{
+				"total_pop": 45914,
+				"avg_HH_size": 2.77,
+				"med_HH_income": 62368,
+				"med_age": 31.1,
+				"pct_home_ownership": 48.3,
+				"people_per_acre": 12.4,
+				"du_per_acre": 4,
+				"total_biz_acre": 0.1,
+				"total_num_employees": 5693,
+				"market_value": 22,
+				"net_acreage": 4168,
+				"total_du": 16642,
+				"total_biz": 435
+			},
+			"centroid":[45.4957,-122.85293]
+		},
+		"Bethany":{
+			"title": "Bethany",
+			"type": "Town Center",
+			"narrative": "The Bethany Town Center is located in northern Washington County, along Bethany Road. The area, in unincorporated Washington County, has no direct highway access and is not serviced by any ODOT facilities. The center is primarily a local retail shopping destination and multi-family housing location. It has 1,641 residents, 649 employees and 868 dwelling units. Bethany Town Center contains 122 gross acres.",
+			"private_amenities": [0,0,1,0,0,2,0,0,2,0,1,2,1,0,4,1],
+			"public_amenities": [0,0,0,0,1],
+			"incomes":[3.3,9.2,7.7,16.6,14.6,48.6],
+			"incomes_1mi_buff": [2.3,7.4,8.1,18.1,17.2,46.8],
+			"emp_breakdown":[21,33,47],
+			"stats":{
+				"total_pop": 1641,
+				"avg_HH_size": 1.98,
+				"med_HH_income": 96870,
+				"med_age": 33.6,
+				"pct_home_ownership": 69.5,
+				"people_per_acre": 21.4,
+				"du_per_acre": 8.1,
+				"total_biz_acre": 0.63,
+				"total_num_employees": 649,
+				"market_value": 50,
+				"net_acreage": 107,
+				"total_du": 868,
+				"total_biz": 67
+			},
+			"stats_1mi_buff":{
+				"total_pop": 45914,
+				"avg_HH_size": 2.77,
+				"med_HH_income": 62368,
+				"med_age": 31.1,
+				"pct_home_ownership": 48.3,
+				"people_per_acre": 12.4,
+				"du_per_acre": 4,
+				"total_biz_acre": 0.1,
+				"total_num_employees": 5693,
+				"market_value": 22,
+				"net_acreage": 4168,
+				"total_du": 16642,
+				"total_biz": 435
+			}
+
+		}
+}
---
+++ gruntfile.js
@@ -0,0 +1,99 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+
+
+          jst: {
+       compile: {
+         options: {
+           namespace: 'templates',
+           processContent: function(src) {
+         return src.replace(/(^\s+|\s+$)/gm, '');
+       },
+       processName: function(filepath) {
+         var t = filepath.replace('templates/','').replace('.htm','');
+         console.log(t);
+         return t;
+       }
+         },
+         files: {
+           "templates/_templates.js": ["templates/*.htm"]
+         }
+       }
+     },
+
+replace: {
+ example: {
+   src: ["templates/_templates.js"],             // source files array (supports minimatch)
+   dest: 'templates/templates.js',             // destination directory or file
+   replacements: [{
+     from: ', __e = _.escape',                   // string replacement
+     to: ''
+   }]
+   }
+   },
+
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          /* App css deps */
+          'css/app.style.min.css': [
+           'bower_components/bootstrap/dist/css/bootstrap.css',
+           'bower_components/fontawesome/css/font-awesome.min.css',
+           'bower_components/mapbox.js/mapbox.css',
+           'css/app.style.css'
+           ]
+        }
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      script: {
+      files: {
+      /* App js deps */
+
+        'js/app.script.min.js' :[
+      'bower_components/modernizr/modernizr.js',
+      'bower_components/jquery/dist/jquery.min.js',
+      'bower_components/bootstrap/dist/js/bootstrap.min.js',
+      'bower_components/mapbox.js/mapbox.js',
+      'bower_components/chartjs/Chart.js',
+      'js/app.script.js',
+        ],
+      }
+      }
+    },
+    
+    copy: {
+      main: {
+        files: [
+          {expand: true,flatten: true, src: ['bower_components/bootstrap/fonts/*'], dest: 'fonts/', filter: 'isFile' },
+          {expand: true,flatten: true, src: ['bower_components/fontawesome/fonts/*'], dest: 'fonts/', filter: 'isFile' },
+           {expand: true,flatten: true, src: ['bower_components/mapbox.js/images/*'], dest: 'css/images/', filter: 'isFile' }
+        ]
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jst');
+  grunt.loadNpmTasks('grunt-text-replace');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  // Default task(s).
+  //grunt.registerTask('default', [ 'jst', 'replace', 'cssmin']);
+ grunt.registerTask('default', [ 'jst','replace','uglify', 'cssmin', 'copy']);
+
+};
